e5cbc23bde4b3c926b406c32f1ab03ab705361f4
yahiro <yahiro07@users.noreply.github.com>
2026-02-18 02:01:15 +0900

    configure logger instance

--- AUv3Framework/Common/Logger.swift
+++ AUv3Framework/Common/Logger.swift
@@ -1,8 +1,1 @@
-class MyLogger {
-  let updLogger = UDPLogger(category: "fw")
-  public init() {}
-  public func log(_ message: String) {
-    updLogger.log(message)
-  }
-}
-let logger = MyLogger()
+let logger = UDPLogger(category: "ext")
