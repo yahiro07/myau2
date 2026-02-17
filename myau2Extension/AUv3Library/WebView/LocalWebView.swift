2ce8309cf6fe4fbb11228ab13db05f73b44e6cff
yahiro <yahiro07@users.noreply.github.com>
2026-02-17 21:53:12 +0900

    load from localhost for debug

--- myau2Extension/AUv3Library/WebView/LocalWebView.swift
+++ myau2Extension/AUv3Library/WebView/LocalWebView.swift
@@ -147,8 +147,14 @@ func commonWebViewSetup(
 
   coordinator.webView = webView
 
-  let url = URL(string: "app://www/index.html")!
-  webView.load(URLRequest(url: url))
+  if false {
+    let url = URL(string: "app://www/index.html")!
+    webView.load(URLRequest(url: url))
+  } else {
+    // For debugging: load from localhost
+    let url = URL(string: "http://localhost:3000")!
+    webView.load(URLRequest(url: url))
+  }
 
   coordinator.callOnBindIfNeeded(onBind)
 
