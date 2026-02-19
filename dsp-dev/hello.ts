53eba9015ce7b7df950ac6dbbb2226aaf6ad8920
yahiro <yahiro07@users.noreply.github.com>
2026-02-19 21:42:06 +0900

    add temporal function

--- dsp-dev/hello.ts
+++ dsp-dev/hello.ts
@@ -1,3 +1,9 @@
 export function greet() {
-	console.log("dsp-dev hello");
+  console.log("dsp-dev hello");
+}
+
+export function processSamples(buffer: Float32Array, len: number) {
+  for (let i = 0; i < len; i++) {
+    buffer[i] = Math.random() * 2 - 1;
+  }
 }
