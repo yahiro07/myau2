9cc88d02502536b0bc2becc0fa71dc4a5bd94a84
yahiro <yahiro07@users.noreply.github.com>
2026-02-19 18:37:44 +0900

    cleanup vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,33 +1,16 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-// import mkcert from "vite-plugin-mkcert";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-const configs = {
-  useDevServerHttps: false,
-  devServerPort: 3000,
-};
-// if (1) {
-//   //AudioWorkletを使用し、スマホなどLAN内でアクセスする場合はhttpsにする
-//   configs.useDevServerHttps = true;
-//   configs.devServerPort = 3002;
-// }
-
 export default defineConfig({
-  appType: "mpa",
-  plugins: [
-    tailwindcss(),
-    react(),
-    tsconfigPaths(),
-    // configs.useDevServerHttps && mkcert(),
-  ],
-  build: {
-    outDir: "./www_dev",
-  },
-  // base: "./",
-  server: {
-    port: configs.devServerPort,
-    host: "0.0.0.0",
-  },
+	appType: "mpa",
+	plugins: [tailwindcss(), react(), tsconfigPaths()],
+	build: {
+		outDir: "./www_dev",
+	},
+	server: {
+		port: 3000,
+		host: "0.0.0.0",
+	},
 });
