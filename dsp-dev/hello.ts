d36a1bd4f46257c80f44cdffff4a00c2cb57d78c
yahiro <yahiro07@users.noreply.github.com>
2026-02-19 22:54:38 +0900

    connect to processing function

--- dsp-dev/hello.ts
+++ dsp-dev/hello.ts
@@ -2,8 +2,23 @@ export function greet() {
   console.log("dsp-dev hello");
 }
 
-export function processSamples(buffer: Float32Array, len: number) {
-  for (let i = 0; i < len; i++) {
-    buffer[i] = Math.random() * 2 - 1;
+let noteActive = false;
+
+export function dspDev_processSamples(buffer: Float32Array, len: number) {
+  if (!noteActive) {
+    return;
   }
+  for (let i = 0; i < len; i++) {
+    buffer[i] = (Math.random() * 2 - 1) * 0.1;
+  }
+}
+
+export function dspDev_noteOn(noteNumber: number, velocity: number) {
+  noteActive = true;
+  console.log("dspDev_noteOn", noteNumber, velocity);
+}
+
+export function dspDev_noteOff(_noteNumber: number) {
+  noteActive = false;
+  console.log("dspDev_noteOff");
 }
--- frontend/src/bridge/core-bridge-dev.ts
+++ frontend/src/bridge/core-bridge-dev.ts
@@ -3,6 +3,7 @@ import {
   CoreBridgeMessageReceiver,
   MessageFromUI,
 } from "@/bridge/core-bridge-types";
+import { createSynthesizerRootWithWorklet } from "@/bridge/dsp-dev-support/worklet-wrapper";
 import { logger } from "@/bridge/logger";
 import { removeArrayItem } from "@/utils/array-utils";
 
@@ -29,15 +30,26 @@ function createOnDummyDataProvider() {
 }
 
 export function createCoreBridgeDev(): CoreBridge {
-  const dummyDataProvider = createOnDummyDataProvider();
-
   logger.warn("using development bridge");
+
+  const dummyDataProvider = createOnDummyDataProvider();
+  const synthesizerRoot = createSynthesizerRootWithWorklet();
 
   function sendMessage(msg: MessageFromUI) {
     logger.log("send", msg);
     dummyDataProvider.handleMessage(msg);
 
     if (msg.type === "uiLoaded") {
+    } else if (msg.type === "setParameter") {
+      synthesizerRoot.setParameter(
+        //msg.paramKey,
+        123, //todo
+        msg.value,
+      );
+    } else if (msg.type === "noteOnRequest") {
+      synthesizerRoot.noteOn(msg.noteNumber, 1);
+    } else if (msg.type === "noteOffRequest") {
+      synthesizerRoot.noteOff(msg.noteNumber);
     }
   }
 
