b4d55f5eadb9d15076a4905df7d83b453b3a0fc7
yahiro <yahiro07@users.noreply.github.com>
2026-02-19 22:11:02 +0900

    make dummy data provider

--- frontend/src/bridge/core-bridge-dev.ts
+++ frontend/src/bridge/core-bridge-dev.ts
@@ -8,18 +8,47 @@ import { removeArrayItem } from "@/utils/array-utils";
 
 //development bridge for DSP prototyping.
 
+function createOnDummyDataProvider() {
+  let receiver: CoreBridgeMessageReceiver | undefined;
+
+  return {
+    handleMessage(msg: MessageFromUI) {
+      if (msg.type === "uiLoaded") {
+      } else if (msg.type === "rpcLoadStateKvsItems") {
+        receiver?.({
+          type: "rpcLoadStateKvsItemsResponse",
+          rpcId: msg.rpcId,
+          items: {},
+        });
+      }
+    },
+    subscribe(fn: CoreBridgeMessageReceiver) {
+      receiver = fn;
+    },
+  };
+}
+
 export function createCoreBridgeDev(): CoreBridge {
-  logger.warn("using development bridge")
+  const dummyDataProvider = createOnDummyDataProvider();
+
+  logger.warn("using development bridge");
 
   function sendMessage(msg: MessageFromUI) {
-    try {
-      logger.log(`send ${JSON.stringify(msg)}`);
-    } catch (e) {
-      logger.error(e);
+    logger.log("send", msg);
+    dummyDataProvider.handleMessage(msg);
+
+    if (msg.type === "uiLoaded") {
     }
   }
 
   const receivers: CoreBridgeMessageReceiver[] = [];
+
+  dummyDataProvider.subscribe((msg) => {
+    logger.log("recv", msg);
+    receivers.forEach((fn) => {
+      fn(msg);
+    });
+  });
 
   function assignReceiver(receiver: CoreBridgeMessageReceiver) {
     receivers.push(receiver);
