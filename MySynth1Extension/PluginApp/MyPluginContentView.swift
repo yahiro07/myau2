00d28749fb8d33bce2e63a795630601fb507a44a
yahiro <yahiro07@users.noreply.github.com>
2026-04-02 16:57:06 +0900

    show static page

--- MySynth1Extension/PluginApp/MyPluginContentView.swift
+++ MySynth1Extension/PluginApp/MyPluginContentView.swift
@@ -13,13 +13,10 @@ struct MyPluginContentView: View {
     VStack {
       // Text("MySynth1Extension 0006")
       LocalWebView { webViewIo in
-        #if DEBUG
-          // webViewIo.loadURL("http://localhost:3000?debug=1")
-          let folder = WebViewHelper.getWebFolderPrioritized("www_dev", "www")
-          webViewIo.loadURL("app://\(folder)/index.html?debug=1")
-        #else
-          webViewIo.loadURL("app://www/index.html")
-        #endif
+        // webViewIo.loadURL("http://localhost:3000?debug=1")
+        // let folder = WebViewHelper.getWebFolderPrioritized("www_dev", "www")
+        // webViewIo.loadURL("app://\(folder)/index.html?debug=1")
+        webViewIo.loadURL("app://www/index.html")
         webViewHub.bindWebViewIo(webViewIo)
       }
     }.border(.green, width: 2).ignoresSafeArea()
