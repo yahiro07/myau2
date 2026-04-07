de1050e4b7f659e5fc3a020dcd1e04ec8918d7e0
yahiro <yahiro07@users.noreply.github.com>
2026-04-07 14:12:54 +0900

    show ids in entry

--- v2-swiftpm/swiftpm/Auv3PluginPackage/Sources/FrameworkMain/Library/PluginAudioUnitViewController.swift
+++ v2-swiftpm/swiftpm/Auv3PluginPackage/Sources/FrameworkMain/Library/PluginAudioUnitViewController.swift
@@ -7,6 +7,28 @@ private let log = Logger(
   subsystem: "com.example.sonic.proto-kit-auv3.Project1Extension",
   category: "AudioUnitViewController")
 
+func osTypeString(_ value: Int) -> String {
+  let n = Int(value)
+  var s = ""
+  for i in (0..<4).reversed() {
+    let shift = i * 8
+    let char = UnicodeScalar((n >> shift) & 0xFF)!
+    s.append(Character(char))
+  }
+  return s
+}
+
+func showEntryInfo(_ componentDescription: AudioComponentDescription) {
+  let bundlePath = Bundle.main.bundlePath
+  let bundleID = Bundle.main.bundleIdentifier ?? "unknown"
+  let type = osTypeString(Int(componentDescription.componentType))
+  let subType = osTypeString(Int(componentDescription.componentSubType))
+  let manufacturer = osTypeString(Int(componentDescription.componentManufacturer))
+  logger.log("Loaded From: \(bundlePath)")
+  logger.log("Bundle ID: \(bundleID)")
+  logger.log("Type: \(type), SubType: \(subType), Manufacturer: \(manufacturer)")
+}
+
 open class PluginAudioUnitViewController: AUViewController, AUAudioUnitFactory {
   var audioUnit: PluginAudioUnit?
 
@@ -55,7 +77,8 @@ open class PluginAudioUnitViewController: AUViewController, AUAudioUnitFactory {
   nonisolated public func createAudioUnit(with componentDescription: AudioComponentDescription)
     throws -> AUAudioUnit
   {
-    print("createAudioUnit 1903")
+    logger.trace("createAudioUnit 1347")
+    showEntryInfo(componentDescription)
 
     return try DispatchQueue.main.sync {
 
